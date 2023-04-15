05e082252344b45e0983febc6bbdcfc02d9fb76e
petersamson-kingori <peters.kingori@student.moringaschool.com>
2023-04-15 16:49:00 +0300

    complete

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -1,6 +1,45 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-  
-  # Add routes
-  
+
+  get '/messages' do
+    messages = Message.order(:created_at)
+    messages.to_json
+  end
+
+  post '/messages' do
+    message = Message.new(body: params[:body], username: params[:username])
+    if message.save
+      message.to_json
+    else
+      status 400
+      { error: "Failed to create message" }.to_json
+    end
+  end
+
+  patch '/messages/:id' do
+    message = Message.find_by(id: params[:id])
+    if message
+      if message.update(body: params[:body])
+        message.to_json
+      else
+        status 400
+        { error: "Failed to update message" }.to_json
+      end
+    else
+      status 404
+      { error: "Message not found" }.to_json
+    end
+  end
+
+  delete '/messages/:id' do
+    message = Message.find_by(id: params[:id])
+    if message
+      message.destroy
+      { message: "Message deleted successfully" }.to_json
+    else
+      status 404
+      { error: "Message not found" }.to_json
+    end
+  end
 end
+
---
+++ chatterbox-server/app/models/Message.rb
@@ -0,0 +1,4 @@
+class Message < ActiveRecord::Base
+    validates :body, presence: true
+    validates :username, presence: true
+  end
